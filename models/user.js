116f7ce1071c6bb70d4640460378ca26c7117539
Lia Kim <j.liakim23@gmail.com>
2023-09-26 23:19:54 -0400

    liabackend - added start code

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("../db/connection");
+
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
